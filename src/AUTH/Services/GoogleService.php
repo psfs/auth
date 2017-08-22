5d3f79bc21eb2c73ecad5be8ae034cb6a3fc6609
Fran López <fran.lopez84@hotmail.es>
2017-08-23 00:01:46 +0200

    Improvements in security and Google Verification service

--- src/AUTH/Services/GoogleService.php
+++ src/AUTH/Services/GoogleService.php
@@ -112,7 +112,7 @@ class GoogleService extends AUTHService {
         $client = $this->getClient($this->base . Router::getInstance()->getRoute('auth-google-callback', false));
         $verification = $client->verifyIdToken($idToken);
         $response = new GoogleCheckDto();
-        if(false !== $response){
+        if(false !== $verification){
             $response->fromArray($verification);
             $response->sub = (string)$response->sub;
             $response->iat = (string)$response->iat;
--- src/AUTH/Types/SessionAuthApi.php
+++ src/AUTH/Types/SessionAuthApi.php
@@ -5,6 +5,8 @@ use AUTH\Models\LoginSessionQuery;
 use AUTH\Types\Base\SessionAuthInterface;
 use PSFS\base\config\Config;
 use PSFS\base\dto\JsonResponse;
+use PSFS\base\Logger;
+use PSFS\base\Security;
 use PSFS\base\types\Api;
 
 /**
@@ -17,6 +19,7 @@ abstract class SessionAuthApi extends Api implements SessionAuthInterface
 
     public function init()
     {
+        Security::getInstance()->checkAdmin(null, null, true);
         parent::init();
         $this->checkAuth();
     }
@@ -26,19 +29,34 @@ abstract class SessionAuthApi extends Api implements SessionAuthInterface
      */
     public function checkAuth()
     {
-        if (!$this->security->isAdmin()) {
-            if(!$this->public && Config::getParam('psfs.auth.enable')) {
-                $token = self::getBearerToken();
-                if (empty($token)) {
-                    return $this->json(new JsonResponse(_('Not authorized, missing Api Token in the request'), false), 412);
-                }
+        if(!$this->public && Config::getParam('psfs.auth.enable')) {
+            $token = self::getBearerToken();
+            if (empty($token) && !$this->security->isAdmin()) {
+                return $this->json(new JsonResponse(_('Not authorized, missing Api Token in the request'), false), 412);
+            } else {
                 $session = LoginSessionQuery::checkToken($token);
-                if (!$session) {
+                if (!$session && !$this->security->isAdmin()) {
                     return $this->json(new JsonResponse(_('Not authorized, token not valid'), false), 401);
-                } else {
+                } elseif(null !== $session) {
                     $this->account = $session->getAccountSession();
+                } elseif(!$this->security->isAdmin()) {
+                    return $this->json(new JsonResponse(_('Not authorized, restricted zone'), false), 412);
                 }
             }
+
+        }
+    }
+
+    protected function hydrateModel($pk)
+    {
+        try {
+            $query = $this->prepareQuery();
+            $this->addFilters($query);
+            $this->checkReturnFields($query);
+            $query->filterByPrimaryKey($pk);
+            $this->model = $this->findPk($query, null);
+        } catch (\Exception $e) {
+            Logger::log(get_class($this) . ': ' . $e->getMessage(), LOG_ERR);
         }
     }
 }
